c291ed9d23bfee9e321789cd1abb88fb7b307424
Svetlana Smirnova <99610969+citrusssoda@users.noreply.github.com>
2022-02-17 15:32:51 +0300

    дополнение комментариев
    дописаны комментарии для наглядности, оформлен вход в админ-панель и профиль пользователя, соответсвующих ролей

--- signin.php
+++ signin.php
@@ -3,17 +3,13 @@
 session_start(); //запуск сессии
 require_once 'connect.php';
 //подключение к БД
-
-
 $login=$_POST['login'];
 $password=$_POST['password'];
 
-$check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login`='$login' and `password`='$password'");
-//echo mysqli_num_rows($check_user);  проверка кол-ва строк с указанными данными
-
+$check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login`='$login' and `password`='$password'");//проверка логина и пароля
 if(mysqli_num_rows($check_user)>0){
     
-    $user = mysqli_fetch_assoc($check_user);
+    $user = mysqli_fetch_assoc($check_user); //внос данных в сессию юзер
     $_SESSION['user'] = [
     "id" => $user['id_user'],
     "fio" => $user['fio'],
