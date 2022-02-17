478a7f6f1f1fa074713703b4036ff78196eb0e0b
Svetlana Smirnova <99610969+citrusssoda@users.noreply.github.com>
2022-02-17 15:34:08 +0300

    дописаны комментарии, названия переменных валидны
    убраны лишние куски кода, дописаны комментарии

--- signup.php
+++ signup.php
@@ -4,18 +4,15 @@ session_start(); //запуск сессии
 require_once 'connect.php';
 //подключение к БД
 
-
-$fio = $_POST['fio'];
-$login=$_POST['login'];
-$email=$_POST['email'];
-$password=$_POST['password'];
-$password_confirm=$_POST['password_confirm'];
+$fio = $_POST['fio'];//переменная с ФИО
+$login=$_POST['login'];//логин
+$email=$_POST['email'];//почта
+$password=$_POST['password'];//пароль
+$password_confirm=$_POST['password_confirm'];//повтор пароля
 
 
 if($password===$password_confirm){
-    
     //если пароли совпадают,то
-
     $sql = "INSERT INTO users (`id_user`, `fio`, `login`, `email`, `password`, `id_role`) VALUES (NULL, '$fio', '$login', '$email', '$password', '2')";
 $res = mysqli_query($connect, $sql) or die ("Ошибка". mysqli_error($connect));
     //$sql = mysqli_query($connect,  "INSERT INTO users ('id_user', 'fio', 'login', 'email', 'password', 'id_role') VALUES (NULL, '$fio', '$login', '$email', '$password', '2')");
